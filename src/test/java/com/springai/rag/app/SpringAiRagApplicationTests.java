606a594a3cd0059b4c5235829bb6bfba82ec6391
SIddharth3791 <spatel3791@gmail.com>
2025-01-22 18:23:49 -0500

    first commit

---
+++ src/test/java/com/springai/rag/app/SpringAiRagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springai.rag.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiRagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
